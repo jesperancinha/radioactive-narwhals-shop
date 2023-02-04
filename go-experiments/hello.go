c0fc2fc255c70abbf587800df886dab09ea1c75a
Joao Esperancinha <jofisaes@gmail.com>
2023-02-04 22:19:52 +0100

    Go updates

---
+++ go-experiments/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+    fmt.Println("Here are some ramblings")
+    fmt.Println("Is my Go style better now? Simpler? Flawless? Can the bonus be considered now? And will you talk to me directly?")
+    fmt.Println("Is mijn Go stijl nu beter? Eenvoudiger? Foutloos? Kan de bonus nu in de rekening gebracht worden? Zal jij nu met me direct praten?")
+}
